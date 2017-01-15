a50fe093c5fc620f3415584e189d25d9fe3c838a
katie lewis <katie@codeforamerica.org>
2017-01-15 14:58:11 -0800

    footer stick and release

--- js/main.js
+++ js/main.js
@@ -1,27 +1,4 @@
-//on page load
-//scroll top distance from top to start of nav. 
-//if header is greater than 640, 
-//sticky header at scrolltop 640
-//else use header.height.
-//see if something like this works  
 
-// may need to add 5% padding-bottom to boddy on scroll to make up for the whitespace 
-
-
-//old header height
-/* 
-
-$(document).ready(function() {
-	if(headerHeight > 450) {
-		$('header').addClass('maxHeader');
-		$('.headerNav').addClass('maxNav');
-	}
-	else {
-		$('header').removeClass('maxHeader');
-		$('.headerNav').removeClass('maxNav');
-	}
-});
-*/
 var navBar = $('.headerNav');
 	navScrolled = ".navScrolled";
 	headerHeight = $('header').height();
@@ -41,3 +18,30 @@ $(window).scroll(function () {
 
 // get the scroll top from the top to the end of the project section
 // add this about the the padding-bottom for project section (whitespace?) so that illustration is seen and not blocked
+
+$(window).on('scroll', function() {
+  var footerStick = $('.footerNav');
+  var $window = $(window);
+
+  var docViewTop = $window.scrollTop(); //top of window position
+  var docViewBottom = docViewTop + $window.height(); //height of window = bottom position
+  var elemTop = footerStick.offset().top; //position of top of footerNav
+  var elemBottom = elemTop + footerStick.height(); //get the bottom position of footer nav from adding the two
+  //var otherElHeights = $('#whiteSpace').height(); + $('.headerNav').height();
+  //var testHeight = docViewBottom - elemTop;
+  //var actualBottom = $(selector).offset().top + $(selector).outerHeight(true);
+ //position of hte top of whiteSpace
+  //var OtherElemBottom = otherElemTop + $('#whiteSpace').height(); //get the bottom position of whiteSpace from adding the two
+  
+  //var testHeight = $window.height() - footerStick.height();
+  var otherElemTop = $('#whiteSpace').offset().top + $('#whiteSpace').outerHeight(true); //positon of the bottom of whitespace
+  var elemTop = $('.footerNav').offset().top; //position of top of footerNav
+
+  if (elemBottom < docViewBottom) { // bottom positin of footer nav < height of window
+    $('.footerNav').addClass('test');
+
+  } else if (otherElemTop >= elemTop) { //bottom position of whiteSpace >= top position of the footer
+  	$('.footerNav').removeClass('test');
+  }
+ });
+
